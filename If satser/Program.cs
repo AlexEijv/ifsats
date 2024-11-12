5fdaa68309548ac2cc9a1a58a775c3f28e8208a3
Alexander Eijvergard <alexander.eijvergard@elev.ga.ntig.se>
2024-11-12 11:06:45 +0100

    Nästan klar

--- If satser/Program.cs
+++ If satser/Program.cs
@@ -1,5 +1,41 @@
-﻿if (6 >= 3)
-{
-    Console.WriteLine("Hello world");
-    Console.ReadLine();
-}
+﻿// if (6 >= 3)
+// {
+//     Console.WriteLine("Hello world");
+//     Console.ReadLine();
+// }
+
+
+// Console.WriteLine("Skriv ditt användarnamn");
+// string namn = Console.ReadLine();
+// Console.WriteLine("Skriv ditt lösenord");
+//string password = Console.ReadLine();
+
+// if (namn == "kalleanka" && password == "12345")
+// {
+//     Console.WriteLine("Welcome!");
+// }
+
+// else
+// {
+//     Console.WriteLine("Wrong username or password");
+// }
+// Console.ReadLine();
+
+// int i = 0;
+// while (i < 32)
+// {
+//     Console.WriteLine("Hello world");
+//     i++;
+// }
+// Console.ReadLine();
+
+// string password = Console.ReadLine();
+
+// while (password != "12345")
+// {
+//     Console.WriteLine("Skriv lösenordet");
+//     password = Console.ReadLine();
+// }
+
+
+
