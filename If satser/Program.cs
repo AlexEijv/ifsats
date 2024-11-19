873d71cff19c0b0d268412ada70962a89470f1e1
Alexander Eijvergard <alexander.eijvergard@elev.ga.ntig.se>
2024-11-19 11:02:45 +0100

    Klar

--- If satser/Program.cs
+++ If satser/Program.cs
@@ -38,4 +38,44 @@
 // }
 
 
+// for (int i = 0; i < 5; i++)
+// {
+//     Console.Write("Skriv in ett tal: ");
+//     int tal = int.Parse(Console.ReadLine());
+
+//     if (tal > 5)
+//     {
+//         Console.WriteLine("Högre än 5!");
+//     }
+// }
+
+Console.WriteLine("Eijvans nummer gissning!!!!");
+Console.WriteLine("Talet är mellan 1 och 100");
+
+while (true)
+{
+    string number = Console.ReadLine();
+    int result;
+    bool lyckad = int.TryParse(number, out result);
+
+    if (result == 7)
+    {
+        Console.WriteLine("Grattis, det är rätt!");
+        break;
+    }
+    if (result < 7 && lyckad == true)
+    {
+        Console.WriteLine("Lite högre");
+    }
+    if (result > 7 && lyckad == true)
+    {
+        Console.WriteLine("Lite mindre");
+    }
+    if (lyckad == false)
+    {
+        Console.WriteLine("Du skrev inget heltal");
+    }
+}
+
+Console.ReadLine();
 
